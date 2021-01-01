f3bb8b79f1a4e4ee11043f61dcf8832711fd10cf
jaideepjaison <53474748+jaideepjaison@users.noreply.github.com>
2021-01-01 08:05:39 +0530

    Update assignment_program.py

--- assignment_program.py
+++ assignment_program.py
@@ -1,11 +1,18 @@
 #Design and Implement the python program for Adding Bullets to Wiki Markup
 
-
 import pyperclip
-text = pyperclip.paste()
 
-lines = text.split('\n')
+pyperclip.copy("hello world")
+
+text=pyperclip.paste()
+
+lines=text.split('\n')
+
 for i in range(len(lines)):
-    lines[i] = '*' + lines[i]
-text = '\n'.join(lines)
+
+lines[i]='*'+lines[i]
+text='\n'.join(lines)
+
 pyperclip.copy(text)
+
+print(text)
